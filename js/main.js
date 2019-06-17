c5aab235c29c16441b011706fb6bea775ebc861c
Kostya <kostya@drokin.ru>
2019-06-17 23:15:31 +0300

    Задание готово

--- js/main.js
+++ js/main.js
@@ -13,24 +13,24 @@ var offerTypes = ['palace', 'flat', 'house', 'bungalo'];
 var nearAd = [];
 
 for (var i = 0; i <= 7; i++) {
-  var offerType = offerTypes[Math.floor(Math.random()*offerTypes.length)];
+  var offerType = offerTypes[Math.floor(Math.random() * offerTypes.length)];
   nearAd.push(
-  {
-    author: {
-      avatar: 'img/avatars/user0' + (i + 1) + '.png'
-    },
+      {
+        author: {
+          avatar: 'img/avatars/user0' + (i + 1) + '.png'
+        },
 
-    offer: {
-      type: offerType
-    },
+        offer: {
+          type: offerType
+        },
 
-    location: {
-      x: getRandom(0, mapWidth),
-      y: getRandom(130, 630)
-    }
-  }
-  )
-};
+        location: {
+          x: getRandom(0, mapWidth),
+          y: getRandom(130, 630)
+        }
+      }
+  );
+}
 
 var listPins = document.querySelector('.map__pins');
 var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
