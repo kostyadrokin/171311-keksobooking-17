1bc6f0fb95cc3ea2f6e92b08fee985223035a3d8
Kostya <kostya@drokin.ru>
2019-06-21 02:15:42 +0300

    Правки от Трэвиса

--- js/main.js
+++ js/main.js
@@ -103,13 +103,13 @@ var formType = document.querySelector('#type');
 formType.addEventListener('change', function () {
   var options = formType.querySelectorAll('option');
   for (var i = 0; i < options.length; i++) {
-    if (options[i].selected && options[i].value == 'bungalo') {
+    if (options[i].selected && options[i].value === 'bungalo') {
       formPrice.setAttribute('min', 0);
-    } if (options[i].selected && options[i].value == 'flat') {
+    } if (options[i].selected && options[i].value === 'flat') {
       formPrice.setAttribute('min', 1000);
-    } if (options[i].selected && options[i].value == 'house') {
+    } if (options[i].selected && options[i].value === 'house') {
       formPrice.setAttribute('min', 5000);
-    } if (options[i].selected && options[i].value == 'palace') {
+    } if (options[i].selected && options[i].value === 'palace') {
       formPrice.setAttribute('min', 10000);
     }
   }
