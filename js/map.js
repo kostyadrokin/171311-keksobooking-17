7ecb80ed82d7185c2209abf51b4365e7a42168a3
Kostya <kostya@drokin.ru>
2019-06-30 13:14:42 +0300

    Разделил и властвую

---
+++ js/map.js
@@ -0,0 +1,18 @@
+'use strict';
+
+(function () {
+  // Отрисовка пинов
+  var listPins = document.querySelector('.map__pins');
+  var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+  window.map = {
+    renderPins: function () {
+      for (var j = 0; j < window.util.nearAd.length; j++) {
+        var pinElement = pinTemplate.cloneNode(true);
+        pinElement.style = 'left: ' + window.util.nearAd[j].location.x + 'px; top: ' + window.util.nearAd[j].location.y + 'px;';
+        pinElement.querySelector('img').src = window.util.nearAd[j].author.avatar;
+        pinElement.querySelector('img').alt = window.util.nearAd[j].offer.type;
+        listPins.appendChild(pinElement);
+      }
+    }
+  }
+})();
---
+++ js/util.js
@@ -0,0 +1,9 @@
+'use strict';
+(function () {
+  var nearAd = [];
+
+  window.util = {
+    nearAd: nearAd,
+    mapWidth: document.querySelector('.map__pins').offsetWidth
+  };
+})();
