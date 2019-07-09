3a2acd2b673e3e1585ca42d9aee2b19499f441b0
Kostya <kostya@drokin.ru>
2019-07-10 01:05:16 +0300

    Праввки Трэвиса

--- js/filterForMap.js
+++ js/filterForMap.js
@@ -1,14 +1,14 @@
 'use strict';
-(function() {
+(function () {
 
   var clearMapPins = function () {
     var mapPinsElement = document.querySelector('.map__pins');
     mapPinsElement.querySelectorAll('.map__pin:not(.map__pin--main)').forEach(function (pin) {
-        mapPinsElement.removeChild(pin);
-      });
+      mapPinsElement.removeChild(pin);
+    });
   };
 
-  var mapFiltersHousingType = document.getElementById("housing-type");
+  var mapFiltersHousingType = document.getElementById('housing-type');
   mapFiltersHousingType.addEventListener('change', function () {
     clearMapPins();
     var selectedHouseType = mapFiltersHousingType.options[mapFiltersHousingType.selectedIndex].value;
@@ -16,7 +16,7 @@
       window.renderPins = window.pins;
       window.map.renderPins();
     } else {
-      window.renderPins = window.pins.filter(function(obj) {
+      window.renderPins = window.pins.filter(function (obj) {
         return obj.offer.type === selectedHouseType;
       });
       window.map.renderPins();
